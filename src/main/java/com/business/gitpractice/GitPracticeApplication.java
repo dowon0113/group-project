4808d21511491b0175c9a5d98c1de7c0cae6e98c
dowon0113 <dowon1235@naver.com>
2025-03-26 18:02:08 +0900

    first commit

---
+++ src/main/java/com/business/gitpractice/GitPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.business.gitpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPracticeApplication {
+
+    public static void main(String[] args) {SpringApplication.run(GitPracticeApplication.class, args);
+        System.out.println("Hello World!");
+    }
+
+}
